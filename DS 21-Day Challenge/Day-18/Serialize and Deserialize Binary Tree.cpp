044789334a88d9950b454acb689006d1dca1aabe
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-01 18:57:05 +0530

    Day-18 DS-II

---
+++ DS 21-Day Challenge/Day-18/Serialize and Deserialize Binary Tree.cpp
@@ -0,0 +1,42 @@
+class Codec {
+public:
+    string serialize(TreeNode* root) {
+        queue<TreeNode*> q;
+        q.push(root);
+        string str;
+        while (!q.empty()) {
+            if (q.front() == nullptr) {
+                str = str + "#,";
+            }
+            else {
+                q.push(q.front()->left);
+                q.push(q.front()->right);
+                str = str + to_string(q.front()->val) + ",";
+            }
+            q.pop();
+        }
+        return str;
+    }
+    TreeNode* deserialize(string data) {
+        TreeNode* root = nullptr;
+        queue<TreeNode**> q;
+        q.push(&root);
+        string::iterator first = data.begin();
+        while (first != data.end()) {
+            TreeNode** pp = q.front();
+            if (*first == '#') {
+                advance(first, 2);
+            }
+            else {
+                string::iterator last = find(first, data.end(), ',');
+                int val = stoi(string(first, last));
+                *pp = new TreeNode(val);
+                q.push(&((*pp)->left));
+                q.push(&((*pp)->right));
+                first = next(last);
+            }
+            q.pop();
+        }
+        return root;
+    }
+};
