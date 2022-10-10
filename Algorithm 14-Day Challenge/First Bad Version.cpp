8a1d64e7385e5e1303ea5e268e5c1e1b3f1e4a50
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-10 13:16:37 +0530

    Day-1 Algo

---
+++ Algorithm 14-Day Challenge/First Bad Version.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        int lo = 1, hi = n, mid;
+        while (lo < hi) {
+            mid = lo + (hi - lo) / 2;
+            if (isBadVersion(mid)) hi = mid;
+            else lo = mid+1;
+        }
+        return lo;
+    }
+};
