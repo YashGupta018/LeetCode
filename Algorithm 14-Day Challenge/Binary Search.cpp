a9e1afde88b852cbea17795c4c1591511ca9bd94
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-10 11:04:30 +0530

    Day-1 Algo

---
+++ Algorithm 14-Day Challenge/Binary Search.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low = 0, high = nums.size() - 1;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[mid] < target)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return -1;
+    }
+};
