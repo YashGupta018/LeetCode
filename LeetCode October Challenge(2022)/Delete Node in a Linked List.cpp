c14704af3abb599e8d41b5397faf533edcb83562
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-13 10:35:57 +0530

    13oct_challenge

---
+++ LeetCode October Challenge(2022)/Delete Node in a Linked List.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    void deleteNode(ListNode* node) {
+        ListNode* temp=node->next;
+        node->val=node->next->val;
+        node->next=node->next->next;
+        delete temp; 
+        
+    }
+};
