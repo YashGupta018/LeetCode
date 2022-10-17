39da0b2c489fc78ffabccfa06d26fe79ceabc4f4
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-17 20:58:12 +0530

    Day-8 Algo

---
+++ Algorithm 14-Day Challenge/Day-8/Populating Next Right Pointers in Each Node.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    Node* connect(Node* root) {
+        if(!root)return root;
+        queue<Node*> q;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            for(int i=0;i<n;i++){
+                Node* x=q.front();
+                q.pop();
+                if(i!=n-1)x->next=q.front();
+                if(x->left)q.push(x->left);
+                if(x->right)q.push(x->right);
+            }
+        }
+        return root;
+    }
+};
