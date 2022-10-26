23cefe91d31182c8467154297b22c0c8846aea75
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-26 23:20:24 +0530

    Day-4 Algo

---
+++ Algorithm 21-Day Challenge/Day-4/Backspace String Compare.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:    
+    string getString(string &str) {
+        int n=str.length(), count=0;
+        string result="";
+        for(int i=n-1; i>=0; i--) {
+            char ch=str[i];
+            if(ch=='#') 
+                count++;
+            else {
+                if(count>0)
+                    count--;
+                else {
+                    result+=ch;
+                }                     
+            }
+        }
+        return result;        
+    }
+    bool backspaceCompare(string S, string T) {
+        return getString(S) == getString(T);
+    }
+};
