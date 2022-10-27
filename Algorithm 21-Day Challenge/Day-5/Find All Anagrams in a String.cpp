4fe6d67884c70478ba524e9e3aa71ac69fb7ac7a
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-10-27 17:51:12 +0530

    Day-5 Algo-II

---
+++ Algorithm 21-Day Challenge/Day-5/Find All Anagrams in a String.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> findAnagrams(string s, string p) {
+        vector<int> pv(26,0), sv(26,0), res;
+        if(s.size() < p.size())
+           return res;
+        for(int i = 0; i < p.size(); ++i)
+        {
+            ++pv[p[i]-'a'];
+            ++sv[s[i]-'a'];
+        }
+        if(pv == sv)
+           res.push_back(0);
+        for(int i = p.size(); i < s.size(); ++i) 
+        {
+            ++sv[s[i]-'a'];
+            --sv[s[i-p.size()]-'a']; 
+            if(pv == sv)
+               res.push_back(i-p.size()+1);
+        }
+        return res;
+    }
+};
