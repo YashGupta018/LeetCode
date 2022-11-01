c332d5c0978d01192c8504258063bd9eada8830d
Yash Gupta <50226252+YashGupta018@users.noreply.github.com>
2022-11-01 18:15:29 +0530

    Day-9 Algo-II

---
+++ Algorithm 21-Day Challenge/Day-9/Subsets.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> ans;
+        int n = nums.size();
+        int num = pow(2, n), i = 0;
+        
+        while(i<num)
+        {
+            vector<int> temp;
+            for(int j=0; j<n; j++)
+            {
+                if(i & 1<<j)
+                    temp.push_back(nums[j]);
+            }
+            ans.push_back(temp);
+            i++;
+        }
+        return ans;
+    }
+};
