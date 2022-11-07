e56f6d145e0369034551e5268f80cdbe0989ad75
YashGupta018 <ygupta2001@gmail.com>
2022-11-07 15:14:44 +0530

    Day-16 Algo-II

---
+++ Algorithm 21-Day Challenge/Day-16/Longest Increasing Subsequence.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    int lengthOfLIS(vector<int>& nums) {
+        const int size = nums.size();
+        if (size == 0) { return 0; } 
+        vector<int> dp(size, 1);
+        int res = 1;
+        for (int i = 1; i < size; ++i) {
+            for (int j = 0; j < i; ++j) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = max(dp[i], dp[j]+1);
+                }
+            }
+            res = max (res, dp[i]);
+        }
+        return res;
+    }
+};
