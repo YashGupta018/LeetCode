be6f95938d743393be0932ec80318c4b6220367a
YashGupta018 <ygupta2001@gmail.com>
2022-11-12 20:10:38 +0530

    Day-21 Algo-II

---
+++ Algorithm 21-Day Challenge/Day-21/Happy Number.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        unordered_set<int> tmp;
+        while(n != 1)
+        {
+            if(tmp.find(n) == tmp.end())
+                tmp.insert(n);
+            else
+                return false;
+            int sum = 0;
+            while(n != 0)
+            {
+                sum += pow(n % 10,2);
+                n = n / 10;
+            }   
+            n = sum;
+        }
+        return true;
+    }
+};
