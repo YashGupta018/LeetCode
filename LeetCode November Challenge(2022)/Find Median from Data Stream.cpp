985a831ffb70d62e9c42657055fabf190a48297b
YashGupta018 <ygupta2001@gmail.com>
2022-11-12 11:04:59 +0530

    12nov_challenge

---
+++ LeetCode November Challenge(2022)/Find Median from Data Stream.cpp
@@ -0,0 +1,34 @@
+class MedianFinder {
+public:
+    priority_queue<int, vector<int>, greater<int> > minHeap;
+	priority_queue<int> maxHeap;
+    MedianFinder(){}
+    void addNum(int num) {
+        if (maxHeap.empty() or maxHeap.top() > num) {
+			maxHeap.push(num);
+		} else {
+			minHeap.push(num);
+		}
+		if (maxHeap.size() > minHeap.size() + 1) {
+			minHeap.push(maxHeap.top());
+			maxHeap.pop();
+		} else if (minHeap.size() > maxHeap.size() + 1) {
+			maxHeap.push(minHeap.top());
+			minHeap.pop();
+		}
+    }
+    double findMedian() {
+        if (maxHeap.size() == minHeap.size()) {
+			if (maxHeap.empty()) {
+				return 0;
+			}
+            else {
+				double avg = (maxHeap.top() + minHeap.top()) / 2.0;
+				return avg;
+			}
+		} 
+        else {
+			return maxHeap.size() > minHeap.size() ? maxHeap.top() : minHeap.top();
+		}
+    }
+};
