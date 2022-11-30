b682fb974de3f1d006f1dedea51a8c24f35036e8
YashGupta018 <ygupta2001@gmail.com>
2022-11-30 15:02:18 +0530

    30nov_challenge

---
+++ LeetCode November Challenge(2022)/Unique Number of Occurrences.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        vector<int> ans;
+        int size = arr.size();
+        int i = 0;
+        sort(arr.begin(),arr.end());
+        while(i<size){
+            int count = 1;
+            for(int j =i+1;j<size;j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+                else{
+                    break;
+                }
+            }
+            ans.push_back(count);
+            i = i+count;
+        }
+        size = ans.size();
+        sort(ans.begin(),ans.end());
+        for(int i = 0;i<size-1;i++){
+            if(ans[i]==ans[i+1]){
+                return false;
+            }
+        }
+        return true;   
+    }
+};
