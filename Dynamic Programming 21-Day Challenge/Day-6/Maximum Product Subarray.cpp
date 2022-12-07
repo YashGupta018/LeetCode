1e48838a5111d7fb4193e14a090f548353deaa28
YashGupta018 <ygupta2001@gmail.com>
2022-12-07 12:35:56 +0530

    Day-6 DP

---
+++ Dynamic Programming 21-Day Challenge/Day-6/Maximum Product Subarray.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        int n = nums.size(), res = nums[0], l = 1, r = 1;
+        for (int i = 0; i < n; i++) {
+            l =  (l) * nums[i];
+            r =  (r) * nums[n - 1 - i];
+            res = max(res, max(l, r));
+            if(l==0)l=1;
+            if(r==0)r=1;
+        }
+        return res;
+    }
+};
