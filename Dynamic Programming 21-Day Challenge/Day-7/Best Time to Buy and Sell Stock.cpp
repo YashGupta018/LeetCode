07ec44f5dfd40f7b843c3c2fd66056f8c8fe3c00
YashGupta018 <ygupta2001@gmail.com>
2022-12-08 15:10:19 +0530

    Day-7 Dp

---
+++ Dynamic Programming 21-Day Challenge/Day-7/Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int minelem=prices[0];
+        for(int i=1;i<prices.size();i++){
+            minelem=min(prices[i],minelem);
+            profit=max(profit,(prices[i]-minelem));
+        }
+        return profit;
+    }
+};
