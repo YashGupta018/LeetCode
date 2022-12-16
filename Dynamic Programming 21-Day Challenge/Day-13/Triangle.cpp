6dc675af49744937113830f61799219d08587fc8
YashGupta018 <ygupta2001@gmail.com>
2022-12-16 20:18:53 +0530

    Day-13 DP

---
+++ Dynamic Programming 21-Day Challenge/Day-13/Triangle.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int minimumTotal(vector<vector<int>>& triangle) {
+	for(int level = 1; level < size(triangle); level++) 
+		for(int i = 0; i <= level; i++)
+            triangle[level][i] += min(triangle[level - 1][min(i, (int)size(triangle[level - 1]) - 1)], triangle[level - 1][max(i - 1, 0)]);
+        return *min_element(begin(triangle.back()), end(triangle.back())); 
+    }
+};
