a104731da2942533a45b3e96e027db2c44cc9370
YashGupta018 <ygupta2001@gmail.com>
2022-12-17 21:44:24 +0530

    17dec_challenge

---
+++ LeetCode December Challenge(2022)/Evaluate Reverse Polish Notation.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& t) {
+        stack<int> st;
+        for(int i=0;i<t.size();i++) {
+            if(t[i]=="+"){
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                st.push(a+b);
+                }
+            else if(t[i]=="*") {
+                long a=st.top();
+                st.pop();
+                long b=st.top();
+                st.pop();
+                st.push(b * a);
+                }
+            else if(t[i]=="-") {
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                st.push(b-a);
+                }
+            else if(t[i]=="/") {
+                int a=st.top();
+                st.pop();
+                int b=st.top();
+                st.pop();
+                st.push(b/a);
+                }
+            else {
+                st.push(stoi(t[i]));
+                }
+            } 
+        return st.top();
+    }
+};
