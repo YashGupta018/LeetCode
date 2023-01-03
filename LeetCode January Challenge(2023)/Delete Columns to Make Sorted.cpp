1cdfca4546cb4e4e35cfe8bcf15a95888f3623ad
YashGupta018 <ygupta2001@gmail.com>
2023-01-03 21:35:43 +0530

    3jan_challenge

---
+++ LeetCode January Challenge(2023)/Delete Columns to Make Sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+    public:
+    int minDeletionSize(vector<string>& strs) {
+        int n = strs.size();
+        int m = strs[0].size();
+        int ans = 0;
+
+        for(int i=0; i<m; i++) {
+            for(int j=0; j<n-1; j++) {
+                if(strs[j][i] > strs[j+1][i]) {
+                    ans++;
+                    break;
+                }
+            }
+        }
+        return ans;
+    }
+};
