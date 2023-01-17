bdfd41fc1a9a908d3876caa372ae3f5826234299
Yash Gupta <ygupta2001@gmail.com>
2023-01-17 11:47:15 +0530

    17jan_challenge

---
+++ LeetCode January Challenge(2023)/Flip String to Monotone Increasing.cpp
@@ -0,0 +1,15 @@
+class Solution {
+    public:
+    int minFlipsMonoIncr(string S) {
+        int count_flip = 0, count_one = 0;
+        for (auto i : S) {
+            if (i == '1')
+                count_one++;
+            else {
+                count_flip++;
+            count_flip = min(count_flip, count_one);
+            }
+        }
+        return count_flip;
+    }
+};
