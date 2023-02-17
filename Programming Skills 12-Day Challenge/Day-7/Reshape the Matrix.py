f76c743b2766cb619c984ece5c15456632a6215c
YashGupta018 <ygupta2001@gmail.com>
2023-02-17 22:22:20 +0530

    Day-7 PS

---
+++ Programming Skills 12-Day Challenge/Day-7/Reshape the Matrix.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def matrixReshape(self, mat, r, c):
+        """
+        :type mat: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        if not mat:
+            return mat
+        col = len(mat[0])
+        row = len(mat)
+        if row * col != r * c:
+            return mat
+        new_mat = [[0 for _ in range(c)] for _ in range(r)]
+        i = 0
+        while i < r * c:
+            new_mat[i // c][ i % c] =  mat[i // col][i % col]
+            i += 1
+        return new_mat
