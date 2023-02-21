d8c6dd7076aa1249052fb7360f01fa50124c4302
YashGupta018 <ygupta2001@gmail.com>
2023-02-21 11:45:05 +0530

    Day-11 PS

---
+++ Programming Skills 12-Day Challenge/Day-11/Valid Anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length()) return false;
+        int n = s.length();
+        unordered_map<char, int> counts;
+        for (int i = 0; i < n; i++) {
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for (auto count : counts)
+            if (count.second) return false;
+        return true;
+    }
+};
