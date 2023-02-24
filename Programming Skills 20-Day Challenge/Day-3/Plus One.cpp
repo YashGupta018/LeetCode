cf1f56bdea22bf42468818e67b2f757205a06431
YashGupta018 <ygupta2001@gmail.com>
2023-02-24 11:45:50 +0530

    Day-3 PS-II

---
+++ Programming Skills 20-Day Challenge/Day-3/Plus One.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> plusOne(vector<int> &digits) {
+        bool carry = true;
+        for(int i=digits.size()-1; i >= 0 && carry; i--) {
+            carry = (++digits[i]%=10) == 0;
+        }
+        if(carry) {
+            digits.insert(digits.begin(), 1);
+        }
+        return digits;
+    }
+};
