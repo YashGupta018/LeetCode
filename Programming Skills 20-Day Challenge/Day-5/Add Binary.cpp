b3df5e58d7f0d60bbf4f8b3107c06c1a46c029d3
YashGupta018 <ygupta2001@gmail.com>
2023-02-26 21:50:25 +0530

    Day-5 PS-II

---
+++ Programming Skills 20-Day Challenge/Day-5/Add Binary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+    int i = a.size()-1, j = b.size()-1;
+    int carry = 0;
+    string ret = "";
+    while (i >= 0 || j >= 0 || carry!=0) {
+        if (i >= 0) {
+            carry += a[i]=='0'?0:1;
+            i--;
+        }
+        if (j >= 0) {
+            carry += b[j]=='0'?0:1;
+            j--;
+        }
+        ret = ((carry%2)==0?"0":"1") + ret;
+        carry /= 2;
+        }
+    return ret;
+    }
+};
