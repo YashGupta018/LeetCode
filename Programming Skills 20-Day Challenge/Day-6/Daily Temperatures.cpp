46a53d9c27766f149c976a75d1c0cd8041d6b35c
YashGupta018 <ygupta2001@gmail.com>
2023-02-27 12:52:14 +0530

    Day-6 PS-II

---
+++ Programming Skills 20-Day Challenge/Day-6/Daily Temperatures.cpp
@@ -0,0 +1,30 @@
+class Solution
+{
+public:
+    vector<int> dailyTemperatures(vector<int> &temperatures)
+    {
+        stack<pair<int, int>> s;
+        int n = temperatures.size();
+        s.push({temperatures[n - 1], n - 1});
+        vector<int> gtr(n);
+        gtr[n - 1] = 0;
+        for (int i = n - 2; i > -1; i--)
+        {
+            while (!s.empty() && s.top().first <= temperatures[i])
+            {
+                s.pop();
+            }
+            if (s.empty())
+            {
+                s.push({temperatures[i], i});
+                gtr[i] = 0;
+            }
+            else
+            {
+                gtr[i] = s.top().second - i;
+                s.push({temperatures[i], i});
+            }
+        }
+        return gtr;
+    }
+};
