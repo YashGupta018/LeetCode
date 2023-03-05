9b1b4132a605a4e93ac0a5b7df2ea262697cbb63
YashGupta018 <ygupta2001@gmail.com>
2023-03-05 20:25:59 +0530

    Day-12 PS-II

---
+++ Programming Skills 20-Day Challenge/Day-12/Subarray Product Less Than K.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int numSubarrayProductLessThanK(vector<int>& nums, int k) {
+        int n = nums.size();
+        if(k <= 1) return 0;
+        int ans = 0;
+        int product = 1;
+        int left = 0;
+        for(int right = 0; right < n; right++) {
+            product *= nums[right];
+            while(product >= k) {
+                product /= nums[left++];
+            }
+            ans += right-left+1;
+        }
+        return ans;
+    }
+};
