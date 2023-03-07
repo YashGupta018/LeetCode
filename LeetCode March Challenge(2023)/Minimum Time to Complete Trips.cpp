65646a76b5ed6bfb5a526c02c8ec69d2c1f7145d
YashGupta018 <ygupta2001@gmail.com>
2023-03-07 23:26:28 +0530

    7mar_challenge

---
+++ LeetCode March Challenge(2023)/Minimum Time to Complete Trips.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        long long left=0,right=totalTrips*(long long)time[0],ans=right;
+        while(left<right) {
+            long long mid = (left+right)/2;
+            long long count=0;
+            for(int i=0;i<time.size();++i) {
+                count+=mid/time[i];
+            }
+            if(count>=totalTrips) {
+                ans=min(ans,mid);
+                right=mid;
+            }
+            else {
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+};
