c9e6424d6beb688c973a53f88cd4fa9e8099d513
YashGupta018 <ygupta2001@gmail.com>
2023-03-13 23:10:14 +0530

    Day-20 PS-II

---
+++ Programming Skills 20-Day Challenge/Day-20/Insert Delete GetRandom O(1).cpp
@@ -0,0 +1,32 @@
+class RandomizedSet {
+    private:
+    vector<int> a;
+    unordered_map<int,int> m;
+    public:
+    RandomizedSet() {
+    }
+    bool insert(int val) {
+        if(m.find(val)!=m.end())
+            return false;
+        else {
+            a.push_back(val); 
+            m[val]=a.size()-1;
+            return true;
+        }
+    }
+    bool remove(int val) {
+        if(m.find(val)==m.end())
+            return false;
+        else {
+            int last=a.back(); 
+            a[m[val]]=a.back();
+            a.pop_back();
+            m[last]=m[val];
+            m.erase(val);
+            return true;
+        }
+    }
+    int getRandom() {
+        return a[rand()%a.size()];
+    }
+};
