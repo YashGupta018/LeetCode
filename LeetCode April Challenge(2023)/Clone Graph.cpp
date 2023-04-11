b031e4dc2750c81e7a6443bc889bf921c68fe335
Yash Gupta <ygupta2001@gmail.com>
2023-04-11 08:28:59 +0530

    9apr_challenge

---
+++ LeetCode April Challenge(2023)/Clone Graph.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    Node* cloneGraph(Node* node) {
+        if (!node) return NULL;
+        unordered_map<Node*, Node*> copies;
+        Node* copy = new Node(node -> val, {});
+        copies[node] = copy;
+        queue<Node*> todo;
+        todo.push(node);
+        while (!todo.empty()) {
+            Node* cur = todo.front();
+            todo.pop();
+            for (Node* neighbor : cur -> neighbors) {
+                if (copies.find(neighbor) == copies.end()) {
+                    copies[neighbor] = new Node(neighbor -> val, {});
+                    todo.push(neighbor);
+                }
+                copies[cur] -> neighbors.push_back(copies[neighbor]);
+            }
+        }
+        return copy;
+    }
+};
