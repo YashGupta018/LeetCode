ba926fc2477fffa59d30a08d2cdbf65015190719
Yash Gupta <ygupta2001@gmail.com>
2023-05-21 18:38:57 +0530

    22apr_challenge

---
+++ LeetCode April Challenge(2023)/Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string& s) {
+        int n = s.size();
+        vector<int> dp(n), dpPrev(n);
+        for (int start = n - 1; start >= 0; --start) {
+            dp[start] = 1;
+            for (int end = start + 1; end < n; ++end) {
+                if (s[start] == s[end]) {
+                    dp[end] = dpPrev[end - 1] + 2;
+                }
+                else {
+                    dp[end] = max(dpPrev[end], dp[end - 1]);
+                }
+            }
+            dpPrev = dp;
+        }
+        return dp[n - 1];
+    }
+    int minInsertions(string s) {
+        return s.length() - longestPalindromeSubseq(s);
+    }
+};
