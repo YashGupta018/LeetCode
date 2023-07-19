87662f88f3897c8f328c7e9a56ce88f7b9b8db26
Yash Gupta <ygupta2001@gmail.com>
2023-07-19 22:27:33 +0530

    19jul_challenge

--- LeetCode July Challenge(2023)/Non-overlapping Intervals.cpp
+++ LeetCode July Challenge(2023)/Non-overlapping Intervals.cpp
@@ -2,13 +2,13 @@ class Solution {
     public int eraseOverlapIntervals(int[][] intervals) {
         int len = intervals.length;
         if(len == 0) return 0;
-        Arrays.sort(intervals, (a, b)->{
+        Arrays.sort(intervals, (a, b)-> {
             return a[1] - b[1];
         });
         int end = intervals[0][1];
         int count = 1;
-        for(int i = 0; i < len; i++){
-            if(intervals[i][0] >= end){
+        for(int i = 0; i < len; i++) {
+            if(intervals[i][0] >= end) {
                 end = intervals[i][1];
                 count++;
             }
