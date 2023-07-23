36ff9ee0104638b8f488d37df1fe34eae6b5607c
Yash Gupta <ygupta2001@gmail.com>
2023-07-23 23:46:58 +0530

    23jul_challenge

---
+++ LeetCode July Challenge(2023)/All Possible Full Binary Trees.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<vector<TreeNode *>> dp;
+    vector<TreeNode *> solve(int n) {
+        if (n % 2 == 0)
+            return {};
+        if (dp[n].size() != 0)
+            return dp[n];
+        if (n == 1) {
+            TreeNode *new_node = new TreeNode(0);
+            return {new_node};
+        }
+        vector<TreeNode *> res;
+        for (int i = 1; i < n; i += 2) {
+            vector<TreeNode *> left = solve(i);
+            vector<TreeNode *> right = solve(n - i - 1);
+            for (TreeNode *l : left) {
+                for (TreeNode *r : right) {
+                    TreeNode *root = new TreeNode(0);
+                    root->left = l;
+                    root->right = r;
+                    res.push_back(root);
+                }
+            }
+        }
+        return dp[n] = res;
+    }
+    vector<TreeNode *> allPossibleFBT(int n) {
+        dp.resize(n + 1);
+        return solve(n);
+    }
+};
