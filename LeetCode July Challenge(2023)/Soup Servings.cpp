e51379182ab3a9d58d533ef6ab2bf81251d91203
Yash Gupta <ygupta2001@gmail.com>
2023-07-29 23:23:44 +0530

    29jul_challenge

---
+++ LeetCode July Challenge(2023)/Soup Servings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    double recur(int a , int b) {
+        if(a <= 0 && b <= 0) return 0.5;
+        if(a <= 0 && b > 0) return 1;
+        if(a > 0 && b <= 0) return 0;
+        double op1 = recur(a - 100 , b);
+        double op2 = recur(a - 75 , b - 25);
+        double op3 = recur(a - 50 , b - 50);
+        double op4 = recur(a - 25 , b - 75);
+        return 0.25 * (op1 + op2 + op3 + op4);
+    }
+    double soupServings(int n) {
+        if(n >= 4800) return 1;
+        double ans = recur(n , n);
+        return ans;
+    }
+};
