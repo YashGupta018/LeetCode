3955b7147f728bb226b89c3a275cba87bd1c24dc
Yash Gupta <ygupta2001@gmail.com>
2023-08-02 23:53:23 +0530

    2aug_challenge

---
+++ LeetCode August Challenge(2023)/Permutations.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<vector<int>> result;
+    void permutation(vector<int> &nums,int i,int n) {
+        if(i==n) {
+            result.push_back(nums);
+            return ;
+        }
+        for(int j=i;j<=n;j++) {
+            swap( nums[i],nums[j]);
+            permutation(nums,i+1,n);
+            swap( nums[i],nums[j]);
+        }
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        permutation(nums,0,nums.size()-1);
+        return result;
+    }
+};
