6d6f6bc64ad00a4147a61633588902da4bed96f0
Yash Gupta <ygupta2001@gmail.com>
2023-08-07 22:57:25 +0530

    7aug_challenge

---
+++ LeetCode August Challenge(2023)/Search a 2D Matrix.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int left = 0, right = m * n - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            int mid_val = matrix[mid / n][mid % n];
+            if (mid_val == target)
+                return true;
+            else if (mid_val < target)
+                left = mid + 1;
+            else
+                right = mid - 1;
+        }
+        return false;
+    }
+};
