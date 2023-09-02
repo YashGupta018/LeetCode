c75cc8ebf5ae6bf7f1a360214ce994e0e5c211e4
Yash Gupta <ygupta2001@gmail.com>
2023-09-02 22:44:48 +0530

    31aug_challenge

--- LeetCode August Challenge(2023)/Minimum Number of Taps to Open to Water a Garden.cpp
+++ LeetCode August Challenge(2023)/Minimum Number of Taps to Open to Water a Garden.cpp
@@ -1,21 +1,23 @@
 class Solution {
-public
-    int minTaps(int n, int[] ranges) {
-        int[] maxReach = new int[n + 1];
-        for (int i = 0; i < ranges.length; i++) {
-            int s = Math.max(0, i - ranges[i]), e = i + ranges[i];
-            maxReach[s] = e;
+public:
+    int minTaps(int n, vector<int> &ranges) {
+        vector<vector<int>> intervals;
+        for (int i = 0; i < ranges.size(); i++) {
+            vector<int> inter{i - ranges[i], i + ranges[i]};
+            intervals.push_back(inter);
         }
-        int tap = 0, currEnd = 0, nextEnd = 0;
-        for (int i = 0; i <= n; i++) {
-            if (i > nextEnd)
-                return -1;
-            if (i > currEnd) {
-                tap++;
-                currEnd = nextEnd;
+        sort(intervals.begin(), intervals.end(), [](auto &p, auto &q)
+        { return (p[0] < q[0]); });
+        int count = 0, start_time = 0, end_time = 0, i = 0;
+        while (end_time < n) {
+            for (; i < intervals.size() && intervals[i][0] <= start_time; i++) {
+                end_time = max(end_time, intervals[i][1]);
             }
-            nextEnd = Math.max(nextEnd, maxReach[i]);
+            if (start_time == end_time)
+                return -1;
+            start_time = end_time;
+            count++;
         }
-        return tap;
+        return count;
     }
-}
+};
